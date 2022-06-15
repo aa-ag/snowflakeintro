c5d9a7f9586f5577f3fe1b31ccea96ad3dd6b82d
aa-ag <aaron.aguerrevere@gmail.com>
2022-06-14 19:46:02 -0500

    Add validate script to validate connection

--- validate.py
+++ validate.py
@@ -1,3 +1,18 @@
 from settings import *
 import snowflake.connector as snc
 
+ctx = snc.connect(
+    user=username,
+    password=thisisnotapassword,
+    account=account
+)
+
+cs = ctx.cursor()
+
+try:
+    cs.execute("SELECT current_version()")
+    one_row = cs.fetchone()
+    print(one_row[0])
+finally:
+    cs.close()
+ctx.close()
