10a6097231bff0fbc47b06591eff9ab8e7af0c39
aa-ag <aaron.aguerrevere@gmail.com>
2022-06-14 20:12:39 -0500

    Environment variables set: validation working as expected

--- validate.py
+++ validate.py
@@ -1,12 +1,15 @@
-from settings import *
+import os
 import snowflake.connector as snc
 
+user=os.environ["USER"]
+password=os.environ["SNOWPASSWORD"]
+account=os.environ["ACCOUNT"]
+
 ctx = snc.connect(
-    user=username,
-    password=thisisnotapassword,
+    user=user,
+    password=password,
     account=account
 )
-
 cs = ctx.cursor()
 
 try:
